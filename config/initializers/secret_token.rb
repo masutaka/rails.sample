fffc748ce3ddf86c286b0ec3515ddc6792b1061c
Takashi Masuda <masutaka.net@gmail.com>
2013-04-14 03:18:45 +0900

    % bundle exec rails new . (Error occued => Could not find gem 'jquery-rails) % bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = '948348676f81fa2ace1c6a05578027ccea08df633296b0dd2f35449128d423d7f28a8791d78e0a7dc75411aabf7173d939db43b8acfaad6ea5f6646b8d6854cc'
